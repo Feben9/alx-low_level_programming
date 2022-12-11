ddc326d5f8bc395b66437df68c26ea0c9cf8bb3a
Feben9 <feben7351@gmail.com>
2022-12-11 01:34:09 -0800

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,8 @@
 /**
  * main - entry point
  * a program to print a string with the printf function
- * Return - always 0 (success)
+ * Return: always 0 (success)
  */
-
 int main(void)
 {
 	printf "with proper grammar, but the outcome is a piece of art,\n";
