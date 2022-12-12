5b0f83198d4dcf83e8f41873137a793b8dd831a8
Feben9 <feben7351@gmail.com>
2022-12-12 00:32:16 -0800

    C - Variables, if, else, while - optional tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-		for (y = 0; y <= 9; y++)
+		for (y = '0'; y <= '9'; y++)
 		{
 			if (y >= x)
 				continue;
