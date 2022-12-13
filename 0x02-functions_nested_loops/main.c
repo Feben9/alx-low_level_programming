eaa590561fc882828dc00d65459e50411817b074
Feben9 <feben7351@gmail.com>
2022-12-13 06:06:11 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+/**
+ * a function to print lowercase alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	       _putchar(x);
+	_putchar('\n');
+	return (void);
+}
