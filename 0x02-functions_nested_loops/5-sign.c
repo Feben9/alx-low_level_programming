26df2db2c82d6d70b74b5f9c6497da3a9a03a196
Feben9 <feben7351@gmail.com>
2022-12-13 11:23:45 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		_putchar(',');
-		_putchar(' ');
 	}
 	if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-		_putchar(',');
-		_putchar(' ');
 	}
 	if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
-		_putchar(',');
-		_putchar(' ');
 	}
 }
