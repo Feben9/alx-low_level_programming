c0ef85d8d23788108a41377c27c956f77d58faa8
Feben9 <feben7351@gmail.com>
2022-12-14 11:27:34 -0800

    C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- entry point
+ * testing whether a number is positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
