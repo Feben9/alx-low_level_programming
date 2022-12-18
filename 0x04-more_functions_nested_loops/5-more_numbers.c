4b9e7cff21109afa39034c921b5dbbe9517e56be
Feben9 <feben7351@gmail.com>
2022-12-17 22:17:20 -0800

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 1; y <= 10; y++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/trialmain.c
+++ 0x04-more_functions_nested_loops/trialmain.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	/*print_most_numbers();*/
-	print_numbers();
+	/*print_numbers();*/
+	more_numbers();
 	return (0);
 }
