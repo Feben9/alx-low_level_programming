d33177f711e734f1e68d802153e0cdd436567003
Feben9 <feben7351@gmail.com>
2022-12-20 08:30:25 -0800

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function to print the integers 0-100
+ * but Fizz for multiples of 3, Buzz for multiples of 5
+ * and FizzBuzz for multiples of both
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", x);
+	}
+	printf('\n');
+}
