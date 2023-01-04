ed7684c8f4fd556f1d03b463bce239c3381d05c2
Feben9 <feben7351@gmail.com>
2023-01-04 10:42:40 -0800

    0x08. C-Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,11 +9,11 @@ int factorial(int n)
 {
 	if (n == 0 || n == 1)
 		return (1);
-	if (n < 0)
-		return (-1);
 	if (n > 0)
 	{
 		n = n * factorial(n - 1);
 		return (n);
 	}
+	else
+		return (-1);
 }
