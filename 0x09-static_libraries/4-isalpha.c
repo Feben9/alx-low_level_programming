85bb9f960c118e545cab69f61ff034a5598ed9e0
Feben9 <feben7351@gmail.com>
2023-01-09 05:35:07 -0800

    0x09. C - Static libraries

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - a function to check if a
+ * character is alphabetic
+ * @c: single letter input
+ * Return: 1 for success 0 for failure
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
