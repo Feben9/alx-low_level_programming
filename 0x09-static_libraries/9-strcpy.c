8d9cfb34b4cd0f12d75c261e1aee922ed87973a1
Feben9 <feben7351@gmail.com>
2023-01-11 11:42:26 -0800

    0x09. C - Static libraries

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - a function that prints n elements of an array of integers
+ * @dest: a single entry
+ * @src: a single entry
+ * Return: a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
 
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+	return (dest);
+}
