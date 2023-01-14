c1b5f0edf397064df08227f6f15317eda979bb0e
Feben9 <feben7351@gmail.com>
2023-01-14 12:57:18 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - a program that prints all the arguments it receives
+ * @argc: count
+ * @argv: string array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 1; count < argc; count++)
+		printf("%s\n", argv[count]);
+
+	return (0);
+}
