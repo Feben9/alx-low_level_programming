5ba993dc58ad34be774a6b74bed39346374f65ea
Feben9 <feben7351@gmail.com>
2023-01-14 12:16:44 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: count
+ * @argv: string array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf(".%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a directory for arguments to main project.
