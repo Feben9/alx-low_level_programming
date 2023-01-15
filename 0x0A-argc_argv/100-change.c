4ae9c3e0454fa88ce3c2b40f5138176d3d60beb2
Feben9 <feben7351@gmail.com>
2023-01-14 16:50:23 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - a program that prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: count
+ * @argv: string array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 1, total, amt;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (atoi(argv[count]) < 0)
+		return (0);
+	amt = atoi(argv[count]);
+	total = amt / 25;
+	amt = amt % 25;
+	total += amt / 10;
+	amt = amt % 10;
+	total += amt / 5;
+	amt = amt % 5;
+	total += amt / 2;
+	amt = amt % 2;
+	total += amt / 1;
+
+	printf("%d\n", total);
+	return (0);
+}
