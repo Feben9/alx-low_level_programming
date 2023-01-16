5a096479edc0db5e4fcb94711c8ea3980d29ef68
Feben9 <feben7351@gmail.com>
2023-01-16 10:23:58 -0800

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a directory for Struct tasks of C - programming.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - defining my first struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description - this is a new struct for learning definition
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
