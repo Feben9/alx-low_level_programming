7d0123a43fe82be72fdf88eb7923d252807e229a
Feben9 <feben7351@gmail.com>
2023-01-17 12:57:33 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,40 @@
 dog_t *new_dog(char *name, float age, char *owner);
 {
 	dog_t *ptr;
+	int i, lname, lowner;
 
 	ptr = malloc(sizeof(*ptr));
-
-	if (ptr)
+	if (p_dog == NULL || !(name) || !(owner))
 	{
-		ptr->name = name;
-		ptr->owner = owner;
-		ptr->age = age;
+		free(p_dog);
+		return (NULL);
 	}
+
+	for (lname = 0; name[lname]; lname++)
+		;
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
 	
-	return(ptr);
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+	
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
