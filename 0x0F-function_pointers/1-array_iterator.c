dd5f99bcd267fb2855545dfae0e420fccea3b40b
Feben9 <feben7351@gmail.com>
2023-01-19 15:41:54 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
-	while (size-- > 0)
+	if (array && action)
 	{
-		action(*array);
-		array++;
+		while (size-- > 0)
+		{
+			action(*array);
+			array++;
+		}
 	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	for (; index < size; index++)
+	if (array && cmp)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
+		for (; index < size; index++)
+		{
+			if (cmp(array[index]) != 0)
+				return (index);
+		}
 	}
 	return (-1);
 }
