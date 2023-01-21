c87f0cf54f83c8fdca5d9b2a0998605292b27e32
Feben9 <feben7351@gmail.com>
2023-01-21 07:23:36 -0800

    0x10. C - Variadic functions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,12 +36,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0; i <= lname; i++)
+	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 	p_dog->age = age;
 
-	for (i = 0; i <= lowner; i++)
+	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
 
